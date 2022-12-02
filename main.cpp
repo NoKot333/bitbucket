48f178357c9f734364b56aa081a720d5448915f0
Nikita <nikitaopsin@gmail.com>
2022-12-02 09:45:09 +0700

    top feature

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
-double my_pow (double num, unsigned int deg) {
-    double rez = 1;
-    for (int i =0; i < deg; i++) {
-        rez*=num;
+double my_pow(long num, long deg) {
+    double result = 1;
+    while(deg) {
+        if (deg % 2 == 0) {
+            deg /= 2;
+            num *= num;
+        }
+        else {
+            deg--;
+            result *= num;
+        }
     }
-    return rez;
+
+    return result;
 }
